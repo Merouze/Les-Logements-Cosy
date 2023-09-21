edcd537365c2b26025dac6b17b7dd38faf6c8ba7
Merouze Aurelien <aurelienmerouze@gmail.com>
2023-09-21 11:36:24 +0200

    create calendar in zenithouse and start function js . up and down month its ok

--- Les-Logements-Cosy-bootstrap-Mérouze-Aurélien/Js/script.js
+++ Les-Logements-Cosy-bootstrap-Mérouze-Aurélien/Js/script.js
@@ -16,24 +16,70 @@ window.onscroll = function () {
 // ************************Animation titre*************************
 
 
-const paragraph = document.querySelector("h1");
+// const paragraph = document.querySelector("h1");
 
-const characters = paragraph.textContent.split("");
+// const characters = paragraph.textContent.split("");
 
-paragraph.textContent = "";
+// paragraph.textContent = "";
 
-// Fonction pour afficher progressivement les caractères
-function animateText(index) {
-  if (index < characters.length) {
-    paragraph.textContent += characters[index];
-    setTimeout(function () {
-      animateText(index + 1);
-    }, 100);
+// // Fonction pour afficher progressivement les caractères
+// function animateText(index) {
+//   if (index < characters.length) {
+//     paragraph.textContent += characters[index];
+//     setTimeout(function () {
+//       animateText(index + 1);
+//     }, 100);
+//   }
+
+// }
+
+// animateText(0);
+
+// *************************Calendar**************************
+let cases = document.getElementsByClassName('case');
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+
+const monthName = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+const UP_MONTH = 'upMonth';
+const DOWN_MONTH = 'downMonth';
+
+function CALENDRIER_REDUCER(action) {
+  switch (action) {
+    case UP_MONTH:
+      if (month < 12) month++
+      else {
+        year++
+        month = 1; // Réinitialisez le mois à janvier
+      }
+      break;
+    case DOWN_MONTH:
+      if (month > 1) month-- // Modifiez la condition pour empêcher le mois de devenir négatif
+      else {
+        year--
+        month = 12; // Réinitialisez le mois à décembre
+      }
+      break;
+    default:
+      break;
   }
-
 }
 
-animateText(0);
+document.getElementById('left').onclick = function () {
+  CALENDRIER_REDUCER(DOWN_MONTH);
+  console.log(month);
+}
+
+document.getElementById('right').onclick = function () {
+  CALENDRIER_REDUCER(UP_MONTH);
+  console.log(month);
+}
+
+
 
 
 // *****************************Form*********************
@@ -42,29 +88,30 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector(".form");
 
   form.addEventListener("submit", function (e) {
-      e.preventDefault(); // Empêche l'envoi par défaut du formulaire
+    e.preventDefault(); // Empêche l'envoi par défaut du formulaire
 
-      // Récupérer les valeurs des champs en utilisant les classes
-      const name = document.querySelector(".name").value;
-      const email = document.querySelector(".email").value;
-      const phoneNumber = document.querySelector(".form-control[type='text']").value;
-      const message = document.querySelector(".message").value;
+    // Récupérer les valeurs des champs en utilisant les classes
+    const name = document.querySelector(".name").value;
+    const email = document.querySelector(".email").value;
+    const phoneNumber = document.querySelector(".form-control[type='text']").value;
+    const message = document.querySelector(".message").value;
 
-      // Créer un objet contenant les données
-      const formData = {
-          name,
-          email,
-          phoneNumber,
-          message,
-      };
+    // Créer un objet contenant les données
+    const formData = {
+      name,
+      email,
+      phoneNumber,
+      message,
+    };
 
-      // Stocker les données dans le Local Storage
-      localStorage.setItem("formData", JSON.stringify(formData));
+    // Stocker les données dans le Local Storage
+    localStorage.setItem("formData", JSON.stringify(formData));
 
-      // Afficher un message de confirmation dans la console
-      console.log("Données stockées dans le Local Storage");
+    // Afficher un message de confirmation dans la console
+    console.log("Données stockées dans le Local Storage");
 
-      // Réinitialiser le formulaire après stockage des données
-      form.reset();
+    // Réinitialiser le formulaire après stockage des données
+    form.reset();
   });
 });
+
